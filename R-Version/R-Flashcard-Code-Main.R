ed17cc5b3bd827801b1c10024afde5db9c4b7fd6
Sphagnaceae <kikirchen@gmail.com>
2026-08-09 02:19:10 -0600

    Update R-Flashcard-Code-Main.R

--- R-Version/R-Flashcard-Code-Main.R
+++ R-Version/R-Flashcard-Code-Main.R
@@ -1,28 +1,23 @@
-#this code makes Anki flashcards from a csv input. See the ReadMe for a 
-#guide on how to use it
+#this code makes Anki flashcards from a csv input. PLEASE SEE THE README FOR A DETAILED GUIDE ON HOW TO USE THIS CODE
 
 #install.packages(dplyr) #do this if you have not installed dplyr 
 library(dplyr)
 
-####---- edit this section---- 
+####---- edit this section-----------------------------------------------
 csvPath <- "C:/PathGoesHere/PlantID_List.csv"
   #remember to format this with forward slashes
-  #In your csv, Family names must be formatted as Latinname (Common) or Latinname, 
-  #for example, Amaranthaceae (Amaranth) or just Amaranthaceae.
 LinesToSkip <- 3
-  #this is the number of lines that should be skipped at the start of your spreadsheet
-  #skip any blank rows. Do NOT skip headers. 
+  #the number of lines to skip at the start of the spreadsheet. skip any blank rows. Do NOT skip headers. 
 FamilyCol <- "Family"
-  #this is the name of the column in which the family names are found
-
+  #the name of your column with family names
+CommonNameCol <- "Common Name"
+LatinNameCol <- "Latin name"
+  #the names of your columns with the latin names and common names
 LabCol <- "Lab"
   #this is the name of the column with the lab/lecture numbers, if applicable
   #this will allow you to tag your cards with the lab or lecture number it came from
-  #you can add the course name in a field below
+  #you can add the course name below
   #If you don't want to use this feature, leave this field blank. 
-CommonNameCol <- "Common Name"
-LatinNameCol <- "Latin name"
-  #like the two others, these are the names of your columns with the latin names and common names
 
 CourseNameTag <- "BIO101::Lab_"
   #this will be followed by the numbering included in LabNumber, for example, BIOL101::Lab_1
@@ -32,11 +27,11 @@ CourseNameTag <- "BIO101::Lab_"
 TaxonomicTag <- "Plants::Vascular_Plants::Dicots::"
   #This is an optional field to add a tag to sort your flashcards by clade
   #remember that Anki tagging systems use the :: characters to denote subfolders
-  #This applies as a character vector to all cards
+  #This applies as a character vector to all cards, and will be followed by the latin family of that card.
 
 txtFileName <- "AnkiCardsTest1"
   #Choose a name for your output .txt file
-####---- 
+####--------------------------------------------------------------------------
 
 csv <- read.csv(file = csvPath, header = TRUE, skip = LinesToSkip)
 
@@ -45,12 +40,13 @@ FamilyCol <- gsub(x = FamilyCol, pattern = " ", replacement = ".")
 LabCol <- gsub(x = LabCol, pattern = " ", replacement = ".")
 CommonNameCol <- gsub(x = CommonNameCol, pattern = " ", replacement = ".")
 LatinNameCol <- gsub(x = LatinNameCol, pattern = " ", replacement = ".")
+
   #Replaces spaces with periods in your inputted column names, since R also does this
   #to the column names of csv that are read in. 
   #If the rest of the code doesn't work for you, run the following:
     #colnames(csv)
-  #then, look at how R has processed your column names, and update the FamilyCol, 
-  #CommonNameCol, etc. variables accordingly in the first section
+  #then, look at how R has processed your column names, and update the original FamilyCol, 
+  #CommonNameCol, etc. variables accordingly
 
 TimeForSomeFlashcardMagic <- function(input = csv, output = txtFileName, 
                                       Latin = LatinNameCol, Common = CommonNameCol,
@@ -64,8 +60,7 @@ TimeForSomeFlashcardMagic <- function(input = csv, output = txtFileName,
     rowCommon <- csv$Common[row]
     
     is_family_common <- grepl(x = rowFamily, pattern = " ")
-    
-    
+
     #The following if/then statement deals with family names. These can be input as 
     #latin name and family name, in the format "Amaranthaceae (Amaranth)", where
     #the parentheses are required, or as latin name only, e.g. "Amaranthaceae".
@@ -73,6 +68,7 @@ TimeForSomeFlashcardMagic <- function(input = csv, output = txtFileName,
       #modify the cat() function in the else statement, replacing "</i>?<br>(LatinName),"
       #with "</i>?<br>(CommonName),". Then, it will work with common names (although)
       #your variable names inside the function will not reflect this)
+    
     if (is_family_common == TRUE){
       message(rowLatin)
       commonIndex <- regexpr(rowFamily, pattern = "\\(")
@@ -90,7 +86,7 @@ TimeForSomeFlashcardMagic <- function(input = csv, output = txtFileName,
         family_latin, ",", TaxoTag, 
         family_latin, " ", Course, Lab, "\n",
         file = outputfile, fill = FALSE, append = TRUE, sep = "")
-      #This section allows you to make flashcards for species without parenthetical 
+      #The above section allows you to make flashcards for species without parenthetical 
         #common names for families (e.g. just as Amaranthaceae instead of 
         #Amaranthaceae (Amaranth))
       }
@@ -98,10 +94,10 @@ TimeForSomeFlashcardMagic <- function(input = csv, output = txtFileName,
     #The next section is the most important section, dealing with common species names
     #and latin species names. 
       #IMPORTANT: If you do not want to learn family names, you can safely delete
-        #all code between lines 79 and 95. 
+        #all code between lines 72 and 88. 
       #IF YOU DO NOT WANT TO LEARN SPECIES COMMON NAMES (or you are dealing with
-          #organisms like mosses, where common names are not readily used), delete
-          #all code between lines 109 and 114. 
+          #organisms like mosses, where common names are not often used), delete
+          #all code between lines 102 and 107. 
     
     cat("What is the <b>common name</b> of <i>", rowLatin, "</i>?,", rowCommon, ",", 
         TaxoTag, family_latin, " ", Course, Lab, "\n",
@@ -117,6 +113,5 @@ outputfile <- paste0(txtFileName, ".txt")
 if(file.exists(outputfile)){
   stop("There is already a file with that name. Please select a different name or continue if you wish to overwrite the old file.")
 }
-
 
 TimeForSomeFlashcardMagic()
